db9b96a063a1ef4f4d9814c3c320e222efbe1338
naxsyambis <naxsyambis@gmail.com>
2025-11-25 15:16:26 +0700

    menambahkan routing CRUD Komik pada API

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,19 @@
+// routers/api.js
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+
 const komikController = require('../controllers/komikController');
+
+// Setup multer untuk upload gambar (disimpan di memory)
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/komik', upload.single('gambar'), komikController.createKomik);
-router.get('/komik', komikController.getAllKomik)
+// ====== KOMIK ROUTES ======
+router.post('/komiks', upload.single('gambar'), komikController.createKomik);
+router.get('/komiks', komikController.getAllKomik);
+router.get('/komiks/:id', komikController.getKomikById);
+router.put('/komiks/:id', upload.single('gambar'), komikController.updateKomik);
+router.delete('/komiks/:id', komikController.deleteKomik);
+
+// Export router untuk digunakan di index.js
+module.exports = router;
